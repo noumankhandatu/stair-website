1ffd96c54442fe318fa90e5001e311e7ac5dd3be
noumankhandatu <noumankhan619.915@gmail.com>
2024-02-12 21:07:06 +0500

    asd

--- src/components/tabs/StairsWidthHeightSliders.jsx
+++ src/components/tabs/StairsWidthHeightSliders.jsx
@@ -135,35 +135,6 @@ const StairsWidthHeightSliders = ({ setAppState, appState }) => {
       </Select>
       {/* hign height */}
 
-      <Appheading sx={{ mt: 2 }}>Hinges Height</Appheading>
-      <Select
-        fullWidth
-        sx={{ height: 40, mt: 1 }}
-        value={appState.svgPlates.leftRightPencilBorder}
-        onChange={(e) => {
-          const action = parseInt(e.target.value);
-          if (!isNaN(action)) {
-            // Check if the selected value is a number
-            const updatedHeight = action;
-            setAppState((prevState) => ({
-              ...prevState,
-              leftRightPencilBorder: {
-                ...prevState.leftRightPencilBorder,
-                height: updatedHeight,
-              },
-            }));
-          }
-        }}
-      >
-        <MenuItem value="" disabled>
-          Select a height
-        </MenuItem>
-        <MenuItem value="1260">180</MenuItem>
-        <MenuItem value="1460">254</MenuItem>
-        <MenuItem value="1660">304</MenuItem>
-        <MenuItem value="1760">324</MenuItem>
-        <MenuItem value="1795">410</MenuItem>
-      </Select>
       {/* Riser */}
       <Appheading sx={{ mt: 2 }}>Remove Add Riser</Appheading>
       <Select
@@ -219,5 +190,37 @@ export default StairsWidthHeightSliders;
             {index} mm
           </MenuItem>
         ))}
+      </Select> */
+}
+
+{
+  /* <Appheading sx={{ mt: 2 }}>Hinges Height</Appheading>
+      <Select
+        fullWidth
+        sx={{ height: 40, mt: 1 }}
+        value={appState.svgPlates.leftRightPencilBorder}
+        onChange={(e) => {
+          const action = parseInt(e.target.value);
+          if (!isNaN(action)) {
+            // Check if the selected value is a number
+            const updatedHeight = action;
+            setAppState((prevState) => ({
+              ...prevState,
+              leftRightPencilBorder: {
+                ...prevState.leftRightPencilBorder,
+                height: updatedHeight,
+              },
+            }));
+          }
+        }}
+      >
+        <MenuItem value="" disabled>
+          Select a height
+        </MenuItem>
+        <MenuItem value="1260">180</MenuItem>
+        <MenuItem value="1460">254</MenuItem>
+        <MenuItem value="1660">304</MenuItem>
+        <MenuItem value="1760">324</MenuItem>
+        <MenuItem value="1795">410</MenuItem>
       </Select> */
 }
